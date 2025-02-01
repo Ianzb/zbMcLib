3d73326ca767feba4b3bf9900a6f323ee13b29a2
Ianzb <93322252@qq.com>
2025-02-01 20:49:18 +0800

    update 0.1.0

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
-from zbMcLib import *
-
-print(MccnVersion().get())
+from zbMcLib import mccn
+mccn.renameAll(r"C:\Users\93322\Downloads\ccui_packs")
+mccn.convertAllBplist(r"C:\Users\93322\Downloads\ccui_packs")
