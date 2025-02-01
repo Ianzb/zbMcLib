60175bd61ba27194d0aa3c73ff159b0a402c367a
Ianzb <93322252@qq.com>
2025-02-01 20:50:03 +0800

    update 0.1.0

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
-from zbMcLib import mccn
-mccn.renameAll(r"C:\Users\93322\Downloads\ccui_packs")
-mccn.convertAllBplist(r"C:\Users\93322\Downloads\ccui_packs")
+from zbMcLib.mccn.ccui import *
+renameAll(r"C:\Users\93322\Downloads\ccui_packs")
+convertAllBplist(r"C:\Users\93322\Downloads\ccui_packs")
