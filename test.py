06340aad17c437cf951981b81a97915e1f2194f4
Ianzb <93322252@qq.com>
2025-02-02 19:55:48 +0800

    add decryptUrl

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
-from zbMcLib.mccn.ccui import *
-renameAll(r"C:\Users\93322\Downloads\ccui_packs")
-convertAllBplist(r"C:\Users\93322\Downloads\ccui_packs")
+from zbMcLib.mccn.url import *
+a=decryptUrl(r"https://g79.gdl.netease.com/mcpe-release2_20250102_3.2-3.2.15.265103-Publish-arm64_r20-encrypted_netease_92_x19.apk?key1=545dcf9ff1e089b62749e569f976a9c3&key2=67a00429")
+print(a)
