84dbbec3afdec655f62dacf5a18e47b03b37018d
Ianzb <93322252@qq.com>
2025-02-12 17:24:01 +0800

    update 0.2.1

--- zbMcLib/mccn/url.py
+++ zbMcLib/mccn/url.py
@@ -9,8 +9,6 @@ def decryptUrl(url: str):
     :param url: 链接
     :return: 带有key1和key2的链接
     """
-    if "g79.gdl.netease.com" not in url:
-        raise Warning("该链接域名很可能无法正常解密。")
     if "?" in url:
         url = url.split("?")[0]
     private_key = "mEE7Cot48r9j2AvEL2N6jpXEc"
@@ -18,6 +16,6 @@ def decryptUrl(url: str):
     expiration_time = current_time + 60 * 60 * 24 * 365
     expiration_time_hex = hex(expiration_time)[2:]
 
-    signature = private_key + urlparse(url).path + expiration_time_hex
+    signature = private_key +url+ expiration_time_hex
     key1 = hashlib.md5(signature.encode()).hexdigest()
     return f"{url}?key1={key1}&key2={expiration_time_hex}"
